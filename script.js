871c36c97559cd53b7370f1f6509603aa87d4860
Jorge-Rosado <108192150+Jorge-Rosado@users.noreply.github.com>
2022-10-25 23:39:44 +0000

    Completed all the javascript taks.

--- script.js
+++ script.js
@@ -1,1 +1,55 @@
+//html element and selector variables
+let p = document.createElement("p");
+let h1 = document.querySelector(".header");
+let input = document.querySelector(".input");
+let userInput;
 
+//declare variable for ghost image
+let ghostImage = document.querySelector(".ghost-image");
+
+
+//declare variables for all of the buttons
+let hideBtn = document.querySelector(".hide-button");
+let showBtn = document.querySelector(".show-button");
+let messageBtn = document.querySelector(".message-button");  
+let updateBtn = document.querySelector(".update-img-button");
+let nameBtn = document.querySelector(".name-button");
+  //create an onclick event for the Hide Me button
+  hideBtn.onclick = function (){  
+    ghostImage.style.display= "none";
+};
+
+//create an onclick event for the Show Me button
+showBtn.onclick = function (){
+  ghostImage.style.display= "initial";
+  
+};
+
+
+//create an onclick event for the Update Img button
+updateBtn.onclick = function (){
+  ghostImage.src = "images/ghost_w_glasses.jpg";
+};
+
+
+//create an onclick event for the Send Message button
+messageBtn.onclick = function(){
+  //update the userInput variable by saving the value of the input
+  let userInput = input.value;
+  //use the insertAdjacentHTML method to append the userInput in a <p> tag
+  let message = document.querySelector(".message");
+  p.innerHTML = userInput;
+  message.insertAdjacentHTML("beforeend", p.outerHTML);
+};
+
+
+
+
+
+//create an onclick event for the Name Me button
+nameBtn.onclick = function(){
+  //update the userInput variable by saving the value of the input
+  let userInput = input.value;
+//use the innerHTML method to update the h1 tag
+  h1.innerHTML = userInput
+};
